036d304322e203e59c225a7039635281069c2ded
ruanvalente <ruanopensuselinux@gmail.com>
2019-10-24 10:51:02 -0300

    M1#40 - Github App: Fazendo o request dos dados do usuário.

--- modulo01/examples/github-app/src/components/search.js
+++ modulo01/examples/github-app/src/components/search.js
@@ -1,10 +1,27 @@
 'use strict'
 
 import React from 'react'
+import ajax from '@fdaciuk/ajax'
 
 const Search = () => (
   <div className='search'>
-    <input type='search' placeholder='Digite seu usuário no Github' />
+    <input
+      type='search'
+      placeholder='Digite seu usuário no Github'
+      onKeyUp={e => {
+        const value = e.target.value
+        const keyCode = e.which || e.keyCode
+        const ENTER = 13
+
+        if (keyCode === ENTER) {
+          ajax()
+            .get(`https://api.github.com/users/${value}`)
+            .then(result => {
+              console.log('Result:', result)
+            })
+        }
+      }}
+    />
   </div>
 )
 
