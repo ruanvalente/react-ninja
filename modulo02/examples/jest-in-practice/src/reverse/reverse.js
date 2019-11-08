b258ce819dc1ed821717b4bd17966e2a3b7cdfb6
ruanvalente <ruanopensuselinux@gmail.com>
2019-11-08 10:29:44 -0300

    M2#A20 - TDD: Reverse.

---
+++ modulo02/examples/jest-in-practice/src/reverse/reverse.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const reverse = (arr) => {
+  let newArray = []
+  
+  for (let i = arr.length; i--;) {
+    newArray.push(arr[i])
+  }
+  return newArray
+
+}
+
+export default reverse
